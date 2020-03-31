5dc828d98853761e1f6bd4261575e9d37201430c
CJ Gaspari <cj.gaspari@gmail.com>
2020-03-31 01:51:10 -0700

    Initial Commit

---
+++ NuTwitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NuTwitter
+//
+//  Created by CJ Gaspari on 3/31/20.
+//  Copyright © 2020 CJ Gaspari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
